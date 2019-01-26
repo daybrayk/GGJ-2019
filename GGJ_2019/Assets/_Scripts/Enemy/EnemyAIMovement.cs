ebf1cefd2a6f9e0e0a706c7ab59d802188c3bf6a
EricBeaulieu <42591911+EricBeaulieu@users.noreply.github.com>
2019-01-25 22:59:40 -0500

    Update

--- GGJ_2019/Assets/_Scripts/Enemy/EnemyAIMovement.cs
+++ GGJ_2019/Assets/_Scripts/Enemy/EnemyAIMovement.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class EnemyAIMovement : AvatarFunctionality {
 
-
+    [SerializeField]
+    private Transform playerLocation;
+    public bool playerInSight;
 
 	// Use this for initialization
-	void Start () {
+	private void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(playerInSight == true)
+        {
+            ChasePlayer();
+        }
+        else
+        {
+            Patrol();
+        }
 	}
+
+    public void ChasePlayer()
+    {
+
+    }
+
+    private void Patrol()
+    {
+
+    }
 }
