edf9bfb51a7459de1f52556b969b37f965f4bad0
EricBeaulieu <42591911+EricBeaulieu@users.noreply.github.com>
2019-01-26 16:03:03 -0500

    Test

---
+++ GGJ_2019/Assets/_Scripts/PlayerScripts/PlayerDestructibleInteractable.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDestructibleInteractable : MonoBehaviour {
+
+    //public CurrentBenefit npcBonus;
+
+    //public delegate void OnNpcChanged(int newNpcNumber);
+    //public static event OnNpcChanged onNpcChanged;
+
+    [SerializeField]
+    private bool canInteract;
+    [SerializeField]
+    private ObstacleType npcNumber;
+    private Destructable destructible;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (canInteract)
+        {
+            if(Input.GetKeyDown(KeyCode.Z))
+            {
+                if(npcNumber == destructible.npcNumber)
+                {
+                    Destroy(destructible.gameObject);
+                    canInteract = false;
+                    destructible = null;
+                }
+            }
+        }
+	}
+
+
+
+    private void OnTriggerEnter(Collider c)
+    {
+        if (c.transform.tag == "Destructible")
+        {
+            canInteract = true;
+            destructible = c.GetComponent<Destructable>();
+        }
+    }
+
+    private void OnTriggerExit(Collider c)
+    {
+        if (c.transform.tag == "Player")
+        {
+            canInteract = false;
+            destructible = null;
+        }
+    }
+
+}
+
+public enum ObstacleType
+{
+    NoNpc, Axe, Fish, Torch, Jacket, Rope
+}
