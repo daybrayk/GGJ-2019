e27b912b06c9e6cdb4b4443676b186091d6b1333
Revelation49 <kurt_huhn@hotmail.com>
2019-01-26 23:36:20 -0500

    New changes for gameplay

---
+++ GGJ_2019/Assets/Kurt/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    static private GameManager gameManager = null;
+
+	// Use this for initialization
+	void Start () {
+		if(_gameManager)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _gameManager = this;
+            DontDestroyOnLoad(this);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Kurts_Workspace");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public static GameManager _gameManager
+    {
+        get { return gameManager; }
+        set { gameManager = value; }
+    }
+}
